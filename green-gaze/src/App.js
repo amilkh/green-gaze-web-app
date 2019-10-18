7c1acad6b521dff55953ebc7401754b51613335c
Bryan Tong <btong@pivotal.io>
2019-10-17 17:31:48 -0700

    Add dummy data

--- green-gaze/src/App.js
+++ green-gaze/src/App.js
@@ -3,7 +3,12 @@ import logo from './logo-gg.png';
 import './App.css';
 // Our imports
 import ImageUploader from 'react-images-upload';
-import {useDropzone} from 'react-dropzone'
+import { CsvToHtmlTable } from 'react-csv-to-table';
+
+const sampleData = `
+id,prob_agriculture,prob_artisinal_mine,prob_bare_ground,prob_blooming,prob_blow_down,prob_clear,prob_cloudy,prob_conventional_mine,prob_cultivation,prob_habitation,prob_haze,prob_partly_clowdy,prob_prime,prob_road,prob_selective_logging,prob_slash_burn,prob_water
+179,0.027891463,1.7747841e-05,0.00012189891,9.149982e-06,0.0010311729,0.002847714,0.48804152,0.0007453955,0.0035745762,0.0034219332,0.72801477,0.07345713,0.6307906,0.022215622,4.9138045e-05,2.4782694e-06,0.09292081
+`;
 
 function App() {
   return (
@@ -14,7 +19,6 @@ function App() {
           <p>
             Upload an image to begin
             <br />
-            Add callback to Juypter / Greenplum for Keras importing here
           </p>
           <ImageUploader withPreview={true}/>
           <a
@@ -23,8 +27,9 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn more about deforestation in the Amazon
           </a>
+          <CsvToHtmlTable data={sampleData} csvDelimiter="," tableClassName="table table-striped table-hover" hasHeader={true}/>
+            Learn more about deforestation in the Amazon
         </div>
         <div className="Footer">
           <a className="Footer-link"
